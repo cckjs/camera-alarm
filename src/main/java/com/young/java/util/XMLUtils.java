37b6113bff42b453767f7ea106d6e0bd62254715
yangyong <398901650@qq.com>
2015-12-30 22:33:08 +0800

    加入了ml处理

---
+++ src/main/java/com/young/java/util/XMLUtils.java
@@ -0,0 +1,21 @@
+package com.young.java.util;
+
+import com.thoughtworks.xstream.XStream;
+
+/**
+ * Created by dell on 2015/12/30.
+ */
+public class XMLUtils {
+
+    private XStream stream;
+
+    public XMLUtils(Class[] classes){
+        stream = new XStream();
+        stream.autodetectAnnotations(true);
+        stream.processAnnotations(classes);
+    }
+
+    public String toXml(Object obj){
+       return stream.toXML(obj);
+    }
+}
